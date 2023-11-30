b357d196ef2634e6b9ed0bc2748a75a6a76a1498
Quvonchbek Bobojonov <moorfoinfo@gmail.com>
2023-11-30 20:43:48 +0500

    chore: args updated

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const getArgs = require('./helpers/args')
 
 const startCLI = () => {
     const args = getArgs(process.argv)
-    console.log(process.argv)
-    console.log(args)
+    if (args.h) {
+        // help
+    }
+    if (args.s) {
+        // save ctiy
+    }
+    if (args.t) {
+        // save token
+    }
+
+    // result
 }
 
 startCLI()
